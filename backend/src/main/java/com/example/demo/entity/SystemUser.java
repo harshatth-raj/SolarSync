45ae4f2ce2bed0861f33c6290c58d03269e6ecda
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-24 08:35:27 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/SystemUser.java
+++ backend/src/main/java/com/example/demo/entity/SystemUser.java
@@ -1,3 +1,12 @@
+import jakarta.annotation.Generated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 public class Systemuser{
-    @
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+    privete Long id;
+    @Column(unique= true)
+    private String 
 }
