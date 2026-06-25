7d2efde3abba4ea4f33f048cc91e8c5f9c14eb37
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 08:36:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/SystemUserRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
-import org.springframework.;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public class SystemUserRepository {
+import com.example.demo.entity.SystemUser;
+
+public interface SystemUserRepository extends JpaRepository<SystemUser, Long> {
     
 }
