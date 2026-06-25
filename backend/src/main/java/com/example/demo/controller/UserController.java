fb4b139e075acc372f142c00859e2b1a0884928e
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 09:44:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -5,4 +5,9 @@ import com.example.demo.service.AuthService;
 public class UserController {
     public final AuthService service;
     
+    public UserController(AuthService service){
+        this.service=service;
+    }
+
+    
 }
