a66e8abf008af52ed6cfdbec6d1293403fe8c257
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 09:26:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/AuthService.java
+++ backend/src/main/java/com/example/demo/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.example.demo.service;
 
+import com.example.demo.repository.SystemUserRepository;
+
 public class AuthService {
-    
+    public final SystemUserRepository repo{
+        this.repo=repo;
+    }
 }
