c60f4857d5937a7a347ef778b14d3cef92fb0fdd
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 08:57:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/RegisterDto.java
+++ backend/src/main/java/com/example/demo/dto/RegisterDto.java
@@ -8,11 +8,45 @@ public class RegisterDto {
     private String email;
     private Role user;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Role getUser() {
+        return user;
+    }
+
+    public void setUser(Role user) {
+        this.user = user;
+    }
+
+
+
     public RegisterDto(String username,String password,String email,Role user ){
        this.username=username;
        this.password=password;
        this.email=email;
-       
+       this.user=user;
 }
 
 }
