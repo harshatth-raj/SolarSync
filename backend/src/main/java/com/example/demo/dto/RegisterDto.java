e57b03416fe5d1eb644bc181b23e1b0a2d5d5b2b
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 09:06:51 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/RegisterDto.java
+++ backend/src/main/java/com/example/demo/dto/RegisterDto.java
@@ -2,16 +2,13 @@ package com.example.demo.dto;
 
 import com.example.demo.enums.Role;
 
-import jakarta.validation.constraints.NotBlank;
+
 
 public class RegisterDto {
-    @NotBlank
+
     private String username;
-    @NotBlank
     private String password;
-    @NotBlank
     private String email;
-    @NotBlank
     private Role user;
 
     public String getUsername() {
