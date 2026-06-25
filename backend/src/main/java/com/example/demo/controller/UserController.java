db5252e10b4584df72b6d17c5cbd16be1b03cf0e
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-06-25 09:56:21 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -2,10 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.RegisterDto;
 import com.example.demo.entity.SystemUser;
 import com.example.demo.service.AuthService;
+
+
+@RestController
 
 public class UserController {
     public final AuthService service;
