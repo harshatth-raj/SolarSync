9cb5ae43538a95f35326c77511b30d9bbcd7c481
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-07-14 08:37:22 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -4,60 +4,54 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.dto.RegisterDto;
 import com.example.demo.entity.SystemUser;
 import com.example.demo.service.AuthService;
 
-
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    public final AuthService service;
-    
-    public UserController(AuthService service){
-        this.service=service;
+
+    private final AuthService service;
+
+    public UserController(AuthService service) {
+        this.service = service;
     }
 
-    @PostMapping("register")
-    public SystemUser createUser(@RequestBody RegisterDto dto){
-        return service.createUser(dto);
+    @PostMapping("/register")
+    public ResponseEntity<SystemUser> register(@RequestBody RegisterDto dto) {
+
+        return new ResponseEntity<>(
+                service.register(dto),
+                HttpStatus.CREATED);
+
     }
 
-    
     @GetMapping
-    public ResponseEntity<List<SystemUser>>getAllUsers(){
-        List<SystemUser>users=service.getAllUsers();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
+    public ResponseEntity<List<SystemUser>> getAllUsers() {
+
+        return ResponseEntity.ok(
+                service.getAllUsers());
+
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SystemUser>getUser(@PathVariable Long id ){
-        SystemUser user =service.getUser(id);
-            return ResponseEntity.status(HttpStatus.OK).body(user);
+    public ResponseEntity<SystemUser> getUserById(@PathVariable Long id) {
+
+        return ResponseEntity.ok(
+                service.getUserById(id));
 
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<SystemUser> UpdateUser(@PathVariable Long id ,@RequestBody RegisterDto dto){
-        SystemUser user = service.UpdateUser(id,dto);
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    
-   @DeleteMapping("/{id}")
-    public ResponseEntity<String>DeleteUser(@PathVariable Long id){
         service.deleteUser(id);
-        return ResponseEntity.status(HttpStatus.OK).body("User deleted Successfully");
+
+        return ResponseEntity.ok("User deleted successfully");
 
     }
+
 }
