5eb1461e30767ad814a08533992984ec063d6555
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-07-15 09:29:24 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,5 +1,24 @@
 package com.example.demo.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", ex.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
 }
