04c14dfeff68f0eb25c31f8b74b390e49232dfe0
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-07-25 12:21:18 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/TicketController.java
+++ backend/src/main/java/com/example/demo/controller/TicketController.java
@@ -1,5 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tickets")
 public class TicketController {
-    
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_MAINTENANCE_TECHNICIAN')")
+    public ResponseEntity<String> getTickets() {
+        return ResponseEntity.ok("Success");
+    }
 }
