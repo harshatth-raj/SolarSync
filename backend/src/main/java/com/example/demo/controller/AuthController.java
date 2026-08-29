9348a01aa9af5f465919dd340393c73110fb0ac2
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-08-29 09:35:14 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.dto.AuthRequestDto;
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(
+            @RequestBody AuthRequestDto dto) {
+
+        return ResponseEntity.ok(
+                authService.login(dto)
+        );
+    }
 }
