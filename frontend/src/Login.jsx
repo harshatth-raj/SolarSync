b6d651e39dc4228a2243bf1e4133d6f0452589fe
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-08-29 08:38:14 +0530

    User commit

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-import { login } from "../store/slices/authSlice";
+import { login } from "./store/slices/authSlice";
 
 function Login() {
 
