8bdfcff31fdfd9924922c24e4c678556b7dc19bb
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-08-29 09:06:44 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 
-import "./index.css";
+import "./App.css";
 
 const root =
     ReactDOM.createRoot(
