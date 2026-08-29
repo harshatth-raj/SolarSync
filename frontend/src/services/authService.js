013ce1a417010ecfb3c2c5bc95c012379107d435
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-08-29 14:31:56 +0530

    User commit

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,35 +1,48 @@
 import api from "./api";
 
 const login = async (userData) => {
-    const response = await api.post("/api/auth/login", userData);
+
+    const response = await api.post(
+        "/api/auth/login",
+        userData
+    );
 
     if (response.data) {
+
         localStorage.setItem(
             "user",
             JSON.stringify(response.data)
         );
+
     }
 
     return response.data;
 };
 
+
 const register = async (userData) => {
+
     const response = await api.post(
-        "/api/user/register",
+        "/api/auth/register",
         userData
     );
 
     return response.data;
 };
 
+
 const logout = () => {
+
     localStorage.removeItem("user");
+
 };
+
 
 const authService = {
     login,
     register,
     logout
 };
+
 
 export default authService;
