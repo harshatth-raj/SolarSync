d92bfe0f3e71a2a887cab0f4c23e5ded3c68bd27
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-02 19:33:45 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,39 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8081",
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: "http://localhost:8081",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use(
-    (config) => {
-        const user = localStorage.getItem("user");
-
-        if (user) {
-            const userData = JSON.parse(user);
-
-            if (userData.token) {
-                config.headers.Authorization = `Bearer ${userData.token}`;
-            }
-        }
-
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            localStorage.removeItem("user");
-            window.location.href = "/login";
-        }
-
-        return Promise.reject(error);
-    }
-);
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
 export default api;
