da0520fc67087bf37a44972a29387c8336e4b57e
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-02 20:16:43 +0530

    User commit

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '../store/slices/authSlice';
+
+export default function Login() {
+  const dispatch = useDispatch();
+  const { error } = useSelector((s) => s.auth);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(login({ username, password }));
+  };
+
+  return (
+    <div className="glass-container">
+      <h1>SolarSync Login</h1>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
