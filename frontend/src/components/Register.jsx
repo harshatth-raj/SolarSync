8cfd7a6a9b9419cd2314e8f5dfeb1983b83ccb1f
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-03 09:06:09 +0530

    User commit

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { register } from "./store/slices/authSlice";
+import { register } from "../store/slices/authSlice";
 
 function Register() {
     const dispatch = useDispatch();
