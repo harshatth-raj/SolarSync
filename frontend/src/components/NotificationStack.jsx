85156c6b3400d3f6a020a2a1b2829aea8e6722bf
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-03 08:43:39 +0530

    User commit

--- frontend/src/components/NotificationStack.jsx
+++ frontend/src/components/NotificationStack.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function NotificationStack({ notifications = [] }) {
   return (
     <div>
-      {notifications.map((n, i) => (
-        <div key={i} className={`notification ${n.type}`}>{n.message}</div>
+      {notifications.map((notification, index) => (
+        <div key={index}>
+          {notification.message}
+        </div>
       ))}
     </div>
   );
