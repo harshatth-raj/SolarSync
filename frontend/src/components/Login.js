4049ad0cd236e31b2a112c340a64eab5419063a0
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-03 09:18:09 +0530

    User commit

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { login } from '../store/slices/authSlice';
 
 export default function Login() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const { error } = useSelector((s) => s.auth);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -16,21 +19,36 @@ export default function Login() {
   return (
     <div className="glass-container">
       <h1>SolarSync Login</h1>
+
       {error && <p>{error}</p>}
+
       <form onSubmit={handleSubmit}>
         <input
           placeholder="Enter your username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
+
         <input
           type="password"
           placeholder="Enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+
         <button type="submit">Login</button>
       </form>
+
+      {/* Register option */}
+      <p>
+        Don't have an account?{' '}
+        <button
+          type="button"
+          onClick={() => navigate('/register')}
+        >
+          Register
+        </button>
+      </p>
     </div>
   );
 }
