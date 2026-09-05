4ce1f59890ca0660df38df01a74d957b470b2eb9
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-05 18:51:32 +0530

    User commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8081";
-
-const api = axios;
+const api = axios.create({
+  baseURL: "http://localhost:8081",
+});
 
 export default api;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,3 +1,4 @@
+
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import siteReducer from './slices/siteSlice';
