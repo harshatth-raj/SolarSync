62fe6fc6a3d1dd7dddb87e59dcde12f429590d21
HARSHATTH RAJ S <727824tucy017@skct.edu.in>
2026-09-05 21:46:35 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/JwtService.java
+++ backend/src/main/java/com/example/demo/service/JwtService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 
 import com.example.demo.util.JwtUtils;
