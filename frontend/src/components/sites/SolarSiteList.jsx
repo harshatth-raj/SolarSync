15a480ae014de602c6ee3cf6fdc9983704abae14
harshatth <harshatth777@gmail.com>
2026-09-12 17:16:10 +0530

    Fix API import path

--- frontend/src/components/sites/SolarSiteList.jsx
+++ frontend/src/components/sites/SolarSiteList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import api from "./services/api";
+import api from "../../services/api";
 import SolarSiteForm from "./SolarSiteForm";
 
 export default function SolarSiteList() {
